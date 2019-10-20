23a54a7c9b4722c068123293d8fcfee075dea023
Arman hossain <armanh688@gmail.com>
2019-10-21 00:32:11 +0600

    responsive contact page

---
+++ responsive-contact.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/slick.css">
+  <link rel="stylesheet" href="css/slick-theme.css>
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/a.png">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+	<title>responsive contact</title>
+</head>
+<body>
+	<!-- start contact section -->
+<section id="contact" class="py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="text-center">
+              Please fill up this form to contact us
+            </h4>
+            <div class="row mt-4">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <input type="text" name="" value="" class="form-control" placeholder="First Name">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <input type="text" name="" value="" class="form-control" placeholder="Last Name">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <input type="text" name="" value="" class="form-control" placeholder="Email">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <input type="text" name="" value="" class="form-control" placeholder="Phone Number">
+                </div>
+              </div>
+              <div class="col-md-12">
+                <div class="form-group">
+                  <textarea name="name" rows="5" cols="80" placeholder="Message" class="form-control"></textarea>
+                </div>
+              </div>
+              <div class="col-md-12">
+                <input type="submit" name="" value="Send" class="btn btn-block btn-dark">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-body">
+            <h4>Get In Touch</h4>
+            <p class="">
+              Lorem ipsum dolor icia, laudantium, tenetur porro!
+            </p>
+            <h4>Addrsss</h4>
+            <p class="">
+              Musapur, Companigang, Noakhali.
+            </p>
+            <h4>Email</h4>
+            <p class="">
+              armanh686@gmail.com
+            </p>
+            <h4>Phone</h4>
+            <p class="">
+              ++001838472540 <br>
+              ++001838472540 <br>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- end contact section -->
+<script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  <script src="js/uikit-icons.min.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
